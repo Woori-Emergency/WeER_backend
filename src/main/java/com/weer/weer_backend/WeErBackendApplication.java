9b48f0e7ee4687b6b3fb4d5a726bc1aab4ec7bbb
SeokCheol <lsc991123@gmail.com>
2024-11-01 09:03:24 +0900

    init

---
+++ src/main/java/com/weer/weer_backend/WeErBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weer.weer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeErBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeErBackendApplication.class, args);
+  }
+
+}
