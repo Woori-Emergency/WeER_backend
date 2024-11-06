1361bf4085d48f5f6056aa4e218967725495eb1e
ayleeee <janhyang0309@gmail.com>
2024-11-06 11:08:24 +0900

    feat:예약 관련 기능 추가

--- src/main/java/com/weer/weer_backend/repository/AdmissionTypeRepository.java
+++ src/main/java/com/weer/weer_backend/repository/AdmissionTypeRepository.java
@@ -1,8 +1,13 @@
 package com.weer.weer_backend.repository;
 
 import com.weer.weer_backend.entity.AdmissionType;
+import com.weer.weer_backend.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AdmissionTypeRepository extends JpaRepository<AdmissionType, Long> {
--- src/main/java/com/weer/weer_backend/repository/UserRepository.java
+++ src/main/java/com/weer/weer_backend/repository/UserRepository.java
@@ -4,8 +4,8 @@ import com.weer.weer_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 사용자 관련 추가 메서드 정의 가능
-
 }
