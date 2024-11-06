739421d971d903a578ae99f6f6b95a20384bee79
dndqlszzz <mandarin1999@naver.com>
2024-11-06 10:45:51 +0900

    refactor: SpringSecurityConfig

--- src/main/java/com/weer/weer_backend/config/SpringSecurityConfig.java
+++ src/main/java/com/weer/weer_backend/config/SpringSecurityConfig.java
@@ -15,16 +15,8 @@ public class SpringSecurityConfig {
         http
                 .csrf().disable() // CSRF 보호 비활성화 (개발용)
                 .authorizeRequests()
-                .requestMatchers("/auth/login", "/auth/signup", "/auth/check-login-id", "/auth/check-email").permitAll() // 인증 없이 접근 허용
-                .anyRequest().authenticated() // 그 외의 요청은 인증 필요
-                .and()
-                .formLogin() // 사용자 지정 로그인 페이지 (필요 시)
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-
+                .requestMatchers("/auth/**","/main").permitAll() // 인증 없이 접근 허용
+                .anyRequest().authenticated(); // 그 외의 요청은 인증 필요
         return http.build();
     }
 }
