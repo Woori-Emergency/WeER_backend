a86cab0f59fc677aaea23354ac4061808f9252a2
박정주 <wnwjdqkr@gmail.com>
2024-11-08 16:05:42 +0900

    refactor: districts 정보 constants로 재사용

--- src/main/java/com/weer/weer_backend/service/EmergencyApiService.java
+++ src/main/java/com/weer/weer_backend/service/EmergencyApiService.java
@@ -1,5 +1,6 @@
 package com.weer.weer_backend.service;
 
+import com.weer.weer_backend.constants.DistrictConstants;
 import com.weer.weer_backend.entity.Emergency;
 import com.weer.weer_backend.entity.Hospital;
 import com.weer.weer_backend.event.DataUpdateCompleteEvent;
@@ -29,12 +30,8 @@ public class EmergencyApiService {
     @Autowired
     private EmergencyRepository emergencyRepository;
 
-    private final List<String> districts = List.of(
-            "강남구", "강동구", "강북구", "강서구", "관악구", "광진구",
-            "구로구", "금천구", "노원구", "도봉구", "동대문구", "동작구", "마포구",
-            "서대문구", "서초구", "성동구", "성북구", "송파구", "양천구", "영등포구",
-            "용산구", "은평구", "종로구", "중구", "중랑구"
-    );
+    private final List<String> districts = DistrictConstants.DISTRICTS;
+
     @Autowired
     private HospitalRepository hospitalRepository;
 
