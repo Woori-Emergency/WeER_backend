12eb89bfe53d39d089aa20a0d4265874043d291d
dndqlszzz <mandarin1999@naver.com>
2024-11-14 16:28:02 +0900

    feat : Add getPatientConditionList

--- src/main/java/com/weer/weer_backend/controller/ReservationController.java
+++ src/main/java/com/weer/weer_backend/controller/ReservationController.java
@@ -1,8 +1,10 @@
 package com.weer.weer_backend.controller;
 
 import com.weer.weer_backend.dto.ApiResponse;
+import com.weer.weer_backend.dto.PatientConditionResponseDTO;
 import com.weer.weer_backend.dto.ReservationDTO;
 import com.weer.weer_backend.dto.ReservationRequestDto;
+import com.weer.weer_backend.entity.PatientCondition;
 import com.weer.weer_backend.entity.Reservation;
 import com.weer.weer_backend.enums.ReservationStatus;
 import com.weer.weer_backend.service.ReservationService;
@@ -45,7 +47,15 @@ public class ReservationController {
     @GetMapping("/hospitals/reservations/{hospitalid}")
     public ApiResponse<List<Reservation>> showHospitalReservation(@PathVariable(name = "hospitalid") Long hospitalid){
         List<Reservation> myReservation = reservationService.getHospitalReservation(hospitalid);
+
         return ApiResponse.success(myReservation);
+    }
+
+    // 병원에 예약 요청을 보낸 환자들의 건강 정보 리스트
+    @GetMapping("/hospitals/patients-info/{patients}")
+    public ApiResponse<List<PatientConditionResponseDTO>> showRequestedPatientsInfo(@PathVariable(name = "patients") List<Long> patientsConditionId){
+
+        return ApiResponse.success(reservationService.getPatientConditionList(patientsConditionId));
     }
 
     @PostMapping("/hospital/reserve")
